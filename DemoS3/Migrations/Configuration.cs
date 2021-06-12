811bc40d6682c353a0f2ca7aa4492c45a12e10e3
Benigno Ranilla <joseb_ranilla@outlook.com>
2021-06-11 20:18:05 -0500

    Validación de datos

---
+++ DemoS3/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace DemoS3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DemoS3.Models.EmpDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DemoS3.Models.EmpDbContext";
+        }
+
+        protected override void Seed(DemoS3.Models.EmpDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- DemoS3/Models/Cliente.cs
+++ DemoS3/Models/Cliente.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace DemoS3.Models
 {
@@ -10,10 +11,15 @@ namespace DemoS3.Models
     {
         public int ID { get; set; }
 
+        [StringLength (60, MinimumLength = 3)]
         public string nombre { get; set; }
 
+        [Display (Name = "Fecha de registro")]
+        [DataType (DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime fechaRegistro { get; set; }
 
+        [Range (18,65)]
         public int edad { get; set; }
 
     }
