6bd047a401433ec7b099c87b328fc41961fa5c3a
Benigno Ranilla <joseb_ranilla@outlook.com>
2021-06-11 19:11:33 -0500

    Add project files.

---
+++ DemoS3/Controllers/ClienteController.cs
@@ -0,0 +1,146 @@
+﻿using DemoS3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoS3.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        public static List<Cliente> empList = new List<Cliente>
+        {
+            new Cliente
+                {
+                    ID = 1,
+                    nombre = "Renato",
+                    fechaRegistro= DateTime.Parse(DateTime.Today.ToString()),
+                    edad = 30
+                },
+
+                new Cliente
+                {
+                    ID = 2,
+                    nombre = "Patricio",
+                    fechaRegistro= DateTime.Parse(DateTime.Today.ToString()),
+                    edad = 32
+                },
+        };
+
+        // GET: Cliente
+        private EmpDbContext db = new EmpDbContext();
+        public ActionResult Index()
+        {
+            var Clientes = from e in db.Cliente
+                           orderby e.ID
+                           select e;
+
+            return View(Clientes);
+
+        }
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        public ActionResult Create(Cliente emp)
+        {
+            try
+            {
+                db.Cliente.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var Clientes = db.Cliente.Single(m => m.ID == id);
+            return View(Clientes);
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                var Cliente = db.Cliente.Single(m => m.ID == id);
+                if (TryUpdateModel(Cliente))
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(Cliente);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [NonAction]
+        public List<Cliente> TodosLosClientes()
+        {
+            return new List<Cliente>
+            {
+                new Cliente
+                {
+                    ID = 1,
+                    nombre = "Renato",
+                    fechaRegistro= DateTime.Parse(DateTime.Today.ToString()),
+                    edad = 30
+                },
+
+                new Cliente
+                {
+                    ID = 2,
+                    nombre = "Patricio",
+                    fechaRegistro= DateTime.Parse(DateTime.Today.ToString()),
+                    edad = 32
+                }
+            };
+        }
+    }
+}
---
+++ DemoS3/Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoS3.Models
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+
+        public string nombre { get; set; }
+
+        public DateTime fechaRegistro { get; set; }
+
+        public int edad { get; set; }
+
+    }
+
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext()
+        { }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+    }
+}
